sini-mary/Contacts-app

Added contacts lists

app/src/main/java/com/sini/contactsapp/contacts.kt
@@ -1,0 +1,9 @@
+package com.sini.contactsapp
+
+data class Contacts(
+    var image:String,
+    var name:String,
+    var phoneNumber:String,
+    var Email:String
+)
+

sini-mary/contactsApp

app/src/main/java/com/sini/contactsapp/contacts.kt
@@ -1,0 +1,10 @@
+package com.sini.contactsapp
+
+data class Contacts(
+    var image: String,
+    var name: String,
+    var phoneNumber: String,
+    var Email:String
+)
+
+